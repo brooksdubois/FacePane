b65e3ec56631c99cd61889204bc1b31908cfb4ce
Brooks DuBois <bdubois2@gmail.com>
2026-06-13 17:33:10 -0400

    Initial Commit

---
+++ WindowPane/WindowPaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowPaneApp.swift
+//  WindowPane
+//
+//  Created by Brooks DuBois on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WindowPaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
