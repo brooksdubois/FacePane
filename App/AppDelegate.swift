48b8d3b1434a7fb74d676a1e4b08e8f514b4541f
Brooks DuBois <bdubois2@gmail.com>
2026-06-13 19:34:49 -0400

    Hiding dock icon and showing icon in top right menu bar

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -2,9 +2,12 @@ import AppKit
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
     private var windowController: WindowController?
+    private var statusItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.regular)
+        NSApp.setActivationPolicy(.accessory)
+
+        setupStatusItem()
 
         let controller = WindowController()
         self.windowController = controller
@@ -12,11 +15,67 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         controller.showWindow(nil)
         controller.window?.makeKeyAndOrderFront(nil)
         controller.window?.orderFrontRegardless()
+    }
 
-        NSApp.activate(ignoringOtherApps: true)
+    private func setupStatusItem() {
+        let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        self.statusItem = item
+
+        if let button = item.button {
+            let image = NSImage(
+                systemSymbolName: "person.crop.rectangle.fill",
+                accessibilityDescription: "WindowPane"
+            )
+            image?.isTemplate = true
+            button.image = image
+            button.imagePosition = .imageOnly
+        }
+
+        let menu = NSMenu()
+
+        menu.addItem(NSMenuItem(
+            title: "Show WindowPane",
+            action: #selector(showWindowPane),
+            keyEquivalent: ""
+        ))
+
+        menu.addItem(NSMenuItem.separator())
+
+        menu.addItem(NSMenuItem(
+            title: "Settings",
+            action: #selector(openSettings),
+            keyEquivalent: ","
+        ))
+
+        menu.addItem(NSMenuItem.separator())
+
+        menu.addItem(NSMenuItem(
+            title: "Quit WindowPane",
+            action: #selector(quit),
+            keyEquivalent: "q"
+        ))
+
+        item.menu = menu
+    }
+
+    @objc private func showWindowPane() {
+        windowController?.showWindow(nil)
+        windowController?.window?.makeKeyAndOrderFront(nil)
+        windowController?.window?.orderFrontRegardless()
+    }
+
+    @objc private func openSettings() {
+        // Placeholder for now.
+        // Later: open SwiftUI Settings window / popover.
+        windowController?.showWindow(nil)
+        windowController?.window?.makeKeyAndOrderFront(nil)
+    }
+
+    @objc private func quit() {
+        NSApp.terminate(nil)
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        true
+        false
     }
 }
